b066cb47db0c46ac87710cdd623a34468a112963
Choudhary-03 <i.am.shakshi.03@gmail.com>
2024-06-26 19:31:04 +0000

    varr

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,13 @@
+const accountId = 123456
+let accountEmail = "shakshi0306@google.com"
+var accountPassword = "4444444"
+accountCity = "Darbhanga"
+//accountId = 2 // not allowed
+
+accountEmail = "kaushik@google.com"
+accountPassword = "1222333"
+accountCity = "surrat"
+
+
+console.log(accountId);
+console.log(accountId, accountEmail, accountPassword, accountCity);
